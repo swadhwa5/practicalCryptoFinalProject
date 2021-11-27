b75e838a4f9092c8ac6aa4e8a8560cf0c6a97a4b
swadhwa5 <swadhwa5@jh.edu>
2021-11-27 00:59:53 -0800

    added code to get password from generated text

--- gpt2.py
+++ gpt2.py
@@ -6,12 +6,28 @@ def main():
     # initialize tokenizer and model from pretrained GPT2 model
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     model = GPT2LMHeadModel.from_pretrained('gpt2')
-    sequence = ""
+    # make user choose the first word
+    sequence = "Hi"
     inputs = tokenizer.encode(sequence, return_tensors='pt')
     # we pass a maximum output length of 200 tokens
-    outputs = model.generate(inputs, max_length=200, do_sample=True, temperature=5.0)
+    outputs = model.generate(inputs, max_length=200, do_sample=True, temperature=4.0)
     text = tokenizer.decode(outputs[0], skip_special_tokens=True)
     print(text)
+    password_words = text.split()[:5] # first 5 words
+    print(password_words)
+    password = []
+    for i in range(5):
+        last_elem = len(password_words[i]) - 1
+        if i == 4:
+            password.append(password_words[i][0])
+        else:
+            password.append(password_words[i][0])
+            password.append(password_words[i][last_elem])
+
+    print(''.join(password))
+
+
+
 
 
 if __name__ == "__main__":
